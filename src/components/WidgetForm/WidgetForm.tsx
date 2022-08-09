e20e98f25c8b6dd36172daf45f0ccc34d5e5e5f4
Guilherme Canella <guycanella@gmail.com>
2022-08-09 20:38:51 -0300

    update readme

--- src/components/WidgetForm/WidgetForm.tsx
+++ src/components/WidgetForm/WidgetForm.tsx
@@ -1,51 +1,49 @@
-import { useState } from "react"
+import { useState } from "react";
 
-import bugImgUrl from '../assets/images/bug.svg'
-import ideaImgUrl from '../assets/images/idea.svg'
-import thoughtImgUrl from '../assets/images/thought.svg'
-import FeedbackTypeStep from "./Steps/FeedbackTypeStep"
-import FeedbackContentStep from "./Steps/FeedbackContentStep"
-import FeedbackSuccessStep from "./Steps/FeedbackSuccessStep"
+import bugImgUrl from "../assets/images/bug.svg";
+import ideaImgUrl from "../assets/images/idea.svg";
+import thoughtImgUrl from "../assets/images/thought.svg";
+import FeedbackTypeStep from "./Steps/FeedbackTypeStep";
+import FeedbackContentStep from "./Steps/FeedbackContentStep";
+import FeedbackSuccessStep from "./Steps/FeedbackSuccessStep";
 
 export const feedbackTypes = {
   bug: {
-    title: 'Problema',
+    title: "Problema",
     image: {
       source: bugImgUrl,
-      alt: 'Imagem de um inseto'
-    }
+      alt: "Imagem de um inseto",
+    },
   },
   idea: {
-    title: 'Ideia',
+    title: "Ideia",
     image: {
       source: ideaImgUrl,
-      alt: 'Imagem de uma lâmpada'
-    }
+      alt: "Imagem de uma lâmpada",
+    },
   },
   other: {
-    title: 'Outro',
+    title: "Outro",
     image: {
       source: thoughtImgUrl,
-      alt: 'Imagem de uma nuvem de pensamento'
-    }
-  }
-}
+      alt: "Imagem de uma nuvem de pensamento",
+    },
+  },
+};
 
-export type FeedbackTypes = keyof typeof feedbackTypes
+export type FeedbackTypes = keyof typeof feedbackTypes;
 
 const WidgetForm = () => {
-  const [feedbackType, setFeedbackType] = useState<FeedbackTypes | null>(null)
-  const [feedbackSent, setFeedbackSent] = useState<boolean>(false)
+  const [feedbackType, setFeedbackType] = useState<FeedbackTypes | null>(null);
+  const [feedbackSent, setFeedbackSent] = useState<boolean>(false);
 
   const handleRestartFeedback = () => {
-    setFeedbackSent(false)
-    setFeedbackType(null)
-  }
+    setFeedbackSent(false);
+    setFeedbackType(null);
+  };
 
   return (
-    <div
-      className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto"
-    >
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
       {feedbackSent ? (
         <FeedbackSuccessStep onFeedbackRestartReq={handleRestartFeedback} />
       ) : (
@@ -61,15 +59,8 @@ const WidgetForm = () => {
           )}
         </>
       )}
-
-      <footer className="text-xs text-neutral-400">
-        Feito com ♥ pela{' '}
-        <a className="underline underline-offset-2" href="https://rocketseat.com.br">
-          Rocketseat
-        </a>
-      </footer>
     </div>
-  )
-}
+  );
+};
 
-export default WidgetForm
+export default WidgetForm;
